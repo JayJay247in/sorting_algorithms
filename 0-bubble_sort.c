df8225d6df4992822f2486f0dd62ce73615cb432
JayJay247in <132079715+JayJay247in@users.noreply.github.com>
2023-10-19 20:44:02 +0100

    corrections.

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,22 +2,28 @@
 #include "sort.h"
 
 void bubble_sort(int *array, size_t size) {
+
+  size_t i, j;
   int swapped;
+
   do {
     swapped = 0;
-    for (size_t i = 1; i < size; i++) {
+    for (i = 1; i < size; i++) {
       if (array[i - 1] > array[i]) {
-        int temp = array[i];
-        array[i] = array[i - 1]; 
-        array[i - 1] = temp;
-        swapped = 1;
         
-        // print array after swap
-        for (size_t j = 0; j < size; j++) {
+        int temp = array[i];
+        array[i] = array[i - 1];
+        array[i - 1] = temp;
+        
+        swapped = 1;
+
+        /*print array after swap*/
+        for (j = 0; j < size; j++) {
           printf("%d ", array[j]);
         }
         printf("\n");
       }
-    }
+    } 
   } while (swapped);
+
 }

92a97ae06f2adc448a92420f1658d6335bafd46d
JayJay247in <132079715+JayJay247in@users.noreply.github.com>
2023-10-23 20:42:16 +0100

    additions and corrections

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,31 +1,35 @@
 #include <stdio.h>
 #include "sort.h"
 
-void shell_sort(int *array, size_t size) {
-  int gap = 1;
+/**
+ * shell_sort - Sort an array using the shell sort algorithm
+ * @array: The array to sort
+ * @size: The size of the array
+ */
+void shell_sort(int *array, size_t size) 
+{
+  size_t gap = 1;
 
   while (gap < size / 3) {
-    gap = gap * 3 + 1;
+    gap = gap * 3 + 1; 
   }
 
   while (gap >= 1) {
-    for (int i = gap; i < size; i++) {
+    size_t i;
+    for (i = gap; i < size; i++) {
       int temp = array[i];
       int j = i;
-      while (j >= gap && array[j - gap] > temp) {
+
+      while ((int)j >= (int)gap && array[j - gap] > temp) {
         array[j] = array[j - gap];
         j -= gap;
       }
+
       array[j] = temp;
     }
     
-    // Print array for current gap  
-    printf("Gap: %d\n", gap);
-    for (int i = 0; i < size; i++) {
-      printf("%d ", array[i]);
-    }
-    printf("\n");
+    print_array(array, size); 
 
-    gap /= 3; 
+    gap /= 3;
   }
 }

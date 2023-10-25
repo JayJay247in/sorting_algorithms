a1237af1a7aaaa27fa1ef71a29c86e54db3207c7
JayJay247in <132079715+JayJay247in@users.noreply.github.com>
2023-10-25 04:52:54 +0100

    addition and correction

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,49 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "sort.h"
 
 #define MAX_DIGITS 10
 
 void radix_sort(int *array, size_t size) {
-  int max = array[0];
-  for (size_t i = 1; i < size; i++) {
-    if (array[i] > max)
-      max = array[i];
-  }
-  
-  int digits = 0;
-  while (max > 0) {
-    max /= 10;
-    digits++;
-  }
-
-  int **buckets = malloc(sizeof(int*) * MAX_DIGITS);
-  for (int i = 0; i < MAX_DIGITS; i++) {
-    buckets[i] = malloc(sizeof(int) * size);
-  }
-  
-  for (int k = 0; k < digits; k++) {
-    int count[MAX_DIGITS] = {0};
-
-    for (size_t i = 0; i < size; i++) {
-      int num = (array[i] / ((int)pow(10, k)) % 10);
-      buckets[num][count[num]] = array[i];
-      count[num]++; 
+    int max = array[0];
+    size_t i;
+    int digits = 0;
+    int j, k;
+    int **buckets;
+    int count[MAX_DIGITS];
+    int idx;
+    
+    for (i = 1; i < size; i++) {
+        if (array[i] > max)
+            max = array[i];
     }
 
-    printf("Sorting digit %d\n", k + 1);
-    print_array(array, size);
-
-    int idx = 0;
-    for (int i = 0; i < MAX_DIGITS; i++) {
-      for(int j = 0; j < count[i]; j++) {
-        array[idx] = buckets[i][j];
-        idx++;
-      }
+    while (max > 0) {
+        max /= 10;
+        digits++;
     }
-  }
 
-  for (int i=0; i < MAX_DIGITS; i++)
-    free(buckets[i]);
-  free(buckets);
+    buckets = (int **)malloc(sizeof(int*) * MAX_DIGITS);
+
+    for (i = 0; i < MAX_DIGITS; i++) {
+        buckets[i] = (int *)malloc(sizeof(int) * size);
+    }
+
+
+    for (k = 0; k < digits; k++) {
+        for (i = 0; i < size; i++) {
+            int num = array[i] / (int)pow(10, k) % 10;
+            buckets[num][count[num]] = array[i];
+            count[num]++;
+        }
+
+        idx = 0;
+
+        for (i = 0; i < MAX_DIGITS; i++) {
+            for (j = 0; j < count[i]; j++) {
+                array[idx] = buckets[i][j];
+                idx++;
+            }
+        }
+
+        for (i = 0; i < MAX_DIGITS; i++) {
+            count[i] = 0;
+        }
+    }
+
+    for (i = 0; i < MAX_DIGITS; i++) {
+        free(buckets[i]);
+    }
+    free(buckets);
 }

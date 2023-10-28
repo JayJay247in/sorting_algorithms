4f66552d70a77fa8148509f1a328ea63498aaa65
JayJay247in <132079715+JayJay247in@users.noreply.github.com>
2023-10-28 03:59:59 +0100

    additons and corrections

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -2,37 +2,63 @@
 #include <math.h>
 #include "sort.h"
 
-void bitonicMerge(int *array, size_t low, size_t cnt, size_t dir) {
-  if (cnt > 1) {
-    size_t k = cnt/2;
-    for (size_t i = low; i < low + k; i++) {
-      if (dir == (array[i] > array[i+k])) {
-        int tmp = array[i];
-        array[i] = array[i+k];
-        array[i+k] = tmp;
-      }
+/*
+ * Merge two bitonic sequences in the array in the specified direction.
+ * Returns 0 on success, non-zero on error.
+*/ 
+int bitonicMerge(int *array, size_t start, size_t length, int direction) {
+  size_t i;
+  size_t mid;
+
+  if (array == NULL) 
+    return 1;
+
+  if (length <= 1) 
+    return 0;
+
+  mid = length / 2;
+
+  /*Merge steps*/ 
+  for (i = start; i < start + mid; i++) {
+    if (direction == (array[i] > array[i+mid])) {
+       /*Swap items*/
+       int temp = array[i];
+       array[i] = array[i+mid];
+       array[i+mid] = temp;
     }
-    bitonicMerge(array, low, k, dir);
-    bitonicMerge(array, low + k, k, dir);
   }
+
+  /*Recursion*/
+  bitonicMerge(array, start, mid, direction); 
+  bitonicMerge(array, start + mid, mid, direction);
+
+  return 0;
 }
 
-void bitonicSort(int *array, size_t low, size_t cnt, size_t dir) {
-  if (cnt > 1) {
-    size_t k = cnt/2;
-    
-    bitonicSort(array, low, k, 1);
-    bitonicSort(array, low+k, k, 0);
-    bitonicMerge(array,low, cnt, dir);
-    
-    // print array
-    for (size_t i = 0; i < cnt; i++) {
-      printf("%d ", array[low + i]);
-    }
-    printf("\n");
-  }
+/*
+ * Sort array segment using bitonic sort algorithm.
+ * Returns 0 on success.
+*/
+int bitonicSort(int *array, size_t start, size_t length, int direction) {
+  size_t mid;
+
+  /*Base case*/
+  if (length <= 1)
+    return 0;
+  
+  mid = length / 2;
+
+  /*Recursively sort halves*/ 
+  bitonicSort(array, start, mid, 1);
+  bitonicSort(array, start + mid, mid, 0);
+
+  /*Merge results*/
+  bitonicMerge(array, start, length, direction); 
+
+  return 0;
 }
 
+/*Driver function*/
 void bitonic_sort(int *array, size_t size) {
-  bitonicSort(array, 0, size, 1); 
+  bitonicSort(array, 0, size, 1);
 }
